c186e40b4098d6ecc2a2490b5608dc455a13f840
Marella Shiva Sai Teja <111701018@smail.iitpkd.ac.in>
2019-10-28 12:02:32 +0530

    completed stage22

---
+++ myexpos/s22.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+cd spl
+
+pwd
+./spl spl_progs/s19exhandler.spl
+./spl spl_progs/s16console_interrupt.spl
+./spl spl_progs/s18disk_interrupt.spl
+./spl spl_progs/s14sample_timer.spl
+./spl spl_progs/MOD0.spl
+./spl spl_progs/MOD1.spl
+./spl spl_progs/MOD1.spl
+./spl spl_progs/s19MOD2.spl
+./spl spl_progs/s18MOD4.spl
+./spl spl_progs/s20MOD5.spl
+./spl spl_progs/MOD7.spl
+./spl spl_progs/s13os_startup.spl
+./spl spl_progs/s16sample_int6.spl
+./spl spl_progs/s15sample_int7.spl
+./spl spl_progs/int8.spl
+./spl spl_progs/s19sample_int9.spl
+./spl spl_progs/s20sample_int10.spl
+./spl spl_progs/s21sample_int11.spl
+./spl spl_progs/s21sample_int15.spl
+./spl spl_progs/int13.spl
+./spl spl_progs/int14.spl
+pwd
+cd ../expl
+./expl expl_progs/s20init.expl
+./expl expl_progs/s12idle.expl
+./expl expl_progs/s21init.expl
+./expl expl_progs/s21as2.expl
+./expl expl_progs/pid.expl
+cd ../xfs-interface
+./xfs-interface
